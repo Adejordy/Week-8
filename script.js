e63f97782b3f258319a42e9acd135c3bbd9a6d9c
Alyssa <alyssadejordy@gmail.com>
2024-09-27 11:29:23 -0700

    add paragraph

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 class Flowers {
-  constructor() {}
+  constructor(type, cost, amount) {
+    this.type = type;
+    this.cost = cost;
+    this.amount = amount;
+  }
+  decribe() {
+    return `This ${this.type} cost ${this.cost} for ${this.amount}`;
+  }
 }
 
 class Cards {
-  constructor() {}
+  constructor(occasion) {
+    this.occasion = occasion;
+  }
+  describe() {
+    return `Add ${this.occasion} to the flower order`;
+  }
 }
