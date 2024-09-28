ec2d61c4d5adc1c8d3184c0f5917cfb3de4caa2a
Alyssa <alyssadejordy@gmail.com>
2024-09-28 13:28:50 -0700

    add paragraph

--- script.js
+++ script.js
@@ -1,24 +1,20 @@
 class Flowers {
-  constructor(amount, type, cost) {
-    this.amount = amount;
+  constructor(type, cost) {
     this.type = type;
     this.cost = cost;
   }
   decribe() {
-    return `a ${this.amount} ${this.type} will cost ${this.cost}`;
+    return `This ${this.type} will cost ${this.cost}`; //This is the return message for the ordered flowers
+  }
+  addOrder(flower) {
+    if (flower instanceof Flowers) {
+      this.flower.push(flower);
+    }
   }
 }
 //I created a class for flowers to order by amount, flower type, and cost,
-class Cards {
-  constructor(occasion) {
-    this.occasion = occasion;
-  }
-  describe() {
-    return `Add ${this.occasion} to the flower order`;
-  }
-}
-//I created a class to add cards to the order by occasion
 class Menu {
+  //This class calls the menu
   constructor() {
     this.flowers = [];
   }
@@ -30,39 +26,43 @@ class Menu {
           this.placeOrder();
           break;
         case "2":
-          this.addCard();
+          this.addNote();
           break;
         case "3":
           this.viewOrder();
           break;
         case "4":
           this.deleteOrder();
+        default:
+          selection = 0;
       }
-    selection = showFlowerOrderOptions();
+    selection = this.showFlowerOrderOptions();
   }
-  showFlowerOrderOptions() {
+  showFlowerOrderOptions(orderInfo) {
     return prompt(`
       1) Place Order
-      2) Add Card
+      2) Add Note
       3) View Order
       4)Delete Order
+      ---------------
+      ${orderInfo}
       `);
   }
-  displayOrder() {
-    let orderString = "";
-    for (let i = 0; i < this.flowers.length; i++) {
-      orderString += i + ") " + this.flowers[i].type;
+  displayFlowers() {
+    let flowerString = "";
+    for (let i = 1; i < this.flowers.length; i++) {
+      flowerString += i + ") " + this.flowers[i];
     }
-    alert(orderString);
+    alert(flowerString);
   }
-  createOrder() {
-    let order = promt("Enter Flower Order");
-    this.flowers.push(new Flowers(order));
+  placeOrder() {
+    let order = promt("Enter Flower Order: ");
+    this.flowers.push(new Flowers(type));
   }
-  viewOrder() {
+  viewFlower() {
     let index = promt("Enter order placed you wish to view:");
-    if (index > -1 && index < this.flowers.length) {
-      this.selectedOrder = this.orders[index];
+    if (index > 0 && index < this.flowers.length) {
+      this.selectedOrder = this.order[index];
       let describe = "The order placed: " + this.selectedOrder.name;
       for (let i = 0; i < this.selectedOrder.name.lenght; i++);
       {
