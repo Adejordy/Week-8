02a1a72fef699fb00f15f1475052cfad746c2989
Alyssa <alyssadejordy@gmail.com>
2024-09-28 16:26:06 -0700

    add paragraph

--- script.js
+++ script.js
@@ -1,34 +1,54 @@
+//Flower order Menu
+
+//class flower order
 class FlowerOrder {
   constructor(type, amount) {
     this.type = type;
     this.amount = amount;
   }
   describe() {
-    return `I want this ${this.type} for and order of ${this.amount}`;
+    return `I want ${this.type} and I want ${this.amount} of them`;
   }
 }
+//class menu
 class Menu {
-  //This class calls the menu
   constructor() {
-    this.order = [];
+    this.orders = []; //order array
   }
-  start() {
-    //Method to start the menu
-    let selection = this.showFlowerOrderOptions();
-    while (selection != 0)
-      switch (selection) {
-        case "1":
-          this.placeOrder();
-          break;
-        case "2":
-          this.viewOrder();
-          break;
-        case "3":
-          this.deleteOrder();
-          break;
-        default:
-          selection = 0;
-      }
-    selection = this.showFlowerOrderOptions();
+  //see menu create menu prompt   //make main menu, make visible with a prompt
+  showFlowerOrdersMenu() {
+    return prompt(`
+      Selection:
+      ------------
+      1) Place Order
+      2) Delete Order
+      3) View Order
+      `);
+  }
+
+  placeOrder() {
+    //place order
+    //promt for flower
+    let flowerType = prompt("Enter Flower Type:");
+    //promt for amount
+    let flowerAmount = prompt("Enter How Many you would like:");
+    this.orders.push(new FlowerOrder(flowerType, flowerAmount));
   }
 }
+
+//push created car into array
+
+//delete order
+//create orderIndex and prompt to enter which index to delete, find method to delete
+
+//veiw order
+//create variable and then loop through the lenght of the array, and then display the variable, create alert
+
+//start menu
+//create new variable selections, list is equal to mainmenu, create switch case add the while != 0
+
+let order1 = new FlowerOrder("Roses", "12");
+console.log(order1);
+let menu = new Menu();
+menu.placeOrder();
+//call all after creating
